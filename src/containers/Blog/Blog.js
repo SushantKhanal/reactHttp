828dde047113db8cd0f95c76fcc62e173dfb651f
Sushant Khanal <dreamerking114@gmail.com>
2019-04-16 16:15:56 +0545

    React router
    replacing anchor tag with Link makes sure the page is not reloaded, but the necessary part is rendered

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,52 +1,32 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 // import axios from 'axios';
+import Posts from './Posts/Posts';
 import axios from '../../axios';
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import NewPost from '../../containers/Blog/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
 
-    state = {
-        posts : [],
-        selectedPostId : null,
-    }
-    
-    componentDidMount() {
-        axios.get("/posts")
-            .then(response=>{
-                const posts = response.data.slice(0,4);
-                const updatedPost = posts.map(post=>{return{
-                    ...post,
-                    author : 'Max',
-                }})
-                this.setState({posts: updatedPost})
-            })
-    }
-
-    postSelectHandler = (id) => {
-        this.setState({selectedPostId: id})
-    }
-
     render () {
-        const posts = this.state.posts.map(post=>
-            <Post clicked={()=>this.postSelectHandler(post.id)}
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-            />)
+
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className='Blog'>
+                <header>
+                    <nav>
+                        <ol>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
+                        </ol>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={()=><Posts/>}/> */}
+                <Route path="/" exact component={Posts}/>
+
+                {/* <Route path="/new-post" exact render={()=><NewPost/>}/> */}
+                <Route path="/new-post" exact component={NewPost}/>
+
             </div>
         );
     }
