8381cd4528daf73cc9e16adc936ad2c6212f2bc8
Sushant Khanal <dreamerking114@gmail.com>
2019-04-17 10:37:57 +0545

    FullPost bug fix
    routing will not unmount the component if only the route parameter has changed like in our FullPost case. So componentDidUpdate becomes necessary

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -11,17 +11,28 @@ class FullPost extends Component {
     }
 
     componentDidMount () {
-        console.log(this.props);
+        this.fetchNewlyLoadedPostHandler()
+    }
+
+    componentDidUpdate () {
+        this.fetchNewlyLoadedPostHandler()
+    }
+
+    fetchNewlyLoadedPostHandler = () => {
         if(!this.state.loadedPost.id || 
-            (this.state.loadedPost.id && this.state.loadedPost.id !== this.props.match.params.id))
-        if(this.props.match.params.id) axios.get('/posts/' + this.props.match.params.id)
-            .then(response => {
-                this.setState({loadedPost:response.data});
-            })
+            (this.state.loadedPost.id 
+                && this.state.loadedPost.id !== +this.props.match.params.id)) {
+                    if(this.props.match.params.id) {
+                        axios.get('/posts/' + this.props.match.params.id)
+                            .then(response => {
+                                this.setState({loadedPost:response.data});
+                            })
+                    }
+                }
     }
 
     deletePostHandler = (id) => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.id)
         .then(response => {
             console.log("this post was deleted, response:", response)
         }, error => {
@@ -34,10 +45,11 @@ class FullPost extends Component {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if(this.state.error){post = <p style={{textAlign: 'center'}}>Something went wrong</p>} 
         else{
-            if(this.props.id)  {
+            if(this.props.match.params.id)  {
                 post = <p style={{textAlign: 'center'}}>Loading...</p>;
             }
             if(this.state.loadedPost)
+            if(this.props.match.params.id == this.state.loadedPost.id)
                 post = (
                     <div className="FullPost">
                         <h1>{this.state.loadedPost.title}</h1>
